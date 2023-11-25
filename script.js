8325ff3d2733f11ea445d1fade77c2a6af6e2da5
Ravi Kiran <113467994+RaviKiran25052@users.noreply.github.com>
2023-11-25 23:41:25 +0530

    api update

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ weatherPart = wrapper.querySelector(".weather-part"),
 wIcon = weatherPart.querySelector("img"),
 arrowBack = wrapper.querySelector("header i");
 
+const my_api_key = 'fd31c9f3ac5116b9b0519c99cb6c516e';
 let api;
 
 inputField.addEventListener("keyup", e =>{
@@ -25,13 +26,14 @@ locationBtn.addEventListener("click", () =>{
 });
 
 function requestApi(city){
-    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=fd31c9f3ac5116b9b0519c99cb6c516e`;
+    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${my_api_key}`;
     fetchData();
 }
 
 function onSuccess(position){
     const {latitude, longitude} = position.coords;
-    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=your_api_key`;
+    // console.log(latitude, longitude);
+    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${my_api_key}`;
     fetchData();
 }
 
